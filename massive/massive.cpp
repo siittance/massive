a60a0226be674b3911ec5b76e5374626c6e1a424
siittance <144900566+siittance@users.noreply.github.com>
2024-02-19 23:36:11 +0300

    Добавьте файлы проекта.

---
+++ massive/massive.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <Windows.h>
+#pragma execution_character_set("utf-8")
+using namespace std;
+
+
+int main() {
+    SetConsoleCP(65001); // устанавливаем кодировку входящего потока данных на UTF-8
+    SetConsoleOutputCP(65001); // устанавливаем кодировку исходящего потока данных на UTF-8
+    const int rows = 3;
+    const int columns = 5;
+    double array[rows][columns];
+    cout << "Введите элементы двумерного массива (3x5):\n";
+
+    for (int i = 0; i < rows; i++) {
+        double sum = 0;
+        for (int j = 0; j < columns; j++) {
+            cout << "Введите строку: ";
+            cin >> array[i][j];
+            sum += array[i][j];
+        }
+        
+        cout << "Среднее арифметическое строки: " << sum / columns << endl;
+    }
+    return 0;
+}
